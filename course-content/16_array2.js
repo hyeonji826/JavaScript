fd1d1d74e9f45bc3532c7a3e27ff07e7356f0177
노현지 <shguswl826@gmail.com>
2025-04-11 12:38:26 +0900

    Dimention

--- course-content/16_array2.js
+++ course-content/16_array2.js
@@ -18,3 +18,21 @@ console.log("-----------------");
 fruits.forEach(function (fruit, index) {
   console.log(`${index}번째 과일: ${fruit}`);
 });
+
+const users = [1, "apple", "김사과", 20, "서울 서초구"];
+
+for (let i = 0; i < users.length; i++) {
+  console.log(users[i]);
+}
+
+console.log("-----------------");
+
+for (let user of users) {
+  console.log(user);
+}
+
+console.log("-----------------");
+
+users.forEach(function (user, idx, arr) {
+  console.log(user, idx, arr);
+});
